3a3ff0a6c86022c1731e7b3f6244b95deef3ab0f
ascarangello <33673583+ascarangello@users.noreply.github.com>
2020-10-01 15:56:51 -0400

    Very basic movement and camera view

---
+++ capstoneProj/Assets/CharController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Adapted from https://www.youtube.com/watch?v=XhliRnzJe5g
+public class CharController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float moveSpeed;
+
+    private Vector3 forward, right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        setMovementVectors();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("HorizontalKey") != 0.0f || Input.GetAxisRaw("VerticalKey") != 0.0f)
+        {
+            Move();
+        }
+        
+    }
+
+    public void setMovementVectors()
+    {
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+    }
+
+    private void Move()
+    {
+        float hor = Input.GetAxis("HorizontalKey");
+        float ver = Input.GetAxis("VerticalKey");
+        Vector3 dir = new Vector3(hor, 0, ver);
+        Vector3 rightMov = right * moveSpeed * Time.deltaTime * hor;
+        Vector3 forwardMov = forward * moveSpeed * Time.deltaTime * ver;
+
+        Vector3 heading = Vector3.Normalize(rightMov + forwardMov);
+        transform.forward = heading;
+        transform.position += rightMov;
+        transform.position += forwardMov;
+
+    }
+}
---
+++ capstoneProj/Assets/Rotator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    TerrainCollider terrain;
+    [SerializeField]
+    float rotSpeed;
+    private Vector3 centerPos;
+    [SerializeField]
+    CharController charController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        centerPos = terrain.bounds.center;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("RotationKey") != 0.0f) {
+            transform.RotateAround(centerPos, Vector3.up, rotSpeed * Time.deltaTime * Input.GetAxisRaw("RotationKey"));
+            charController.setMovementVectors();
+        }
+
+    }
+}
