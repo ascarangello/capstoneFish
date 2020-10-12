2abf88a5f0825e6734b4dcf8cb9fec7c2d9fd917
ascarangello <33673583+ascarangello@users.noreply.github.com>
2020-10-12 13:39:08 -0400

    interactable popup system, not dialogue

--- capstoneProj/Assets/Scripts/InteractablePopup.cs
+++ capstoneProj/Assets/Scripts/InteractablePopup.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class InteractablePopup : MonoBehaviour
 {
+    public GameObject onInteract;
     private PopupManager popup;
+    private bool playerPresence;
     public string displayText;
     // Start is called before the first frame update
     void Start()
@@ -15,16 +17,32 @@ public class InteractablePopup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerPresence && !onInteract.activeSelf && Input.GetButtonDown("Interact"))
+        {
+            popup.clearText();
+            onInteract.SetActive(true);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        popup.setText(displayText);
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            playerPresence = true;
+            popup.setText(displayText);
+        }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        popup.clearText();
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerPresence = false;
+            popup.clearText();
+            if(onInteract.activeSelf)
+            {
+                onInteract.SetActive(false);
+            }
+        }
     }
 }
