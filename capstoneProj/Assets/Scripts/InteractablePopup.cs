6254a258ef4945e2a496589ec4a98b55a2589e90
ascarangello <33673583+ascarangello@users.noreply.github.com>
2020-10-12 13:17:52 -0400

    Popup text

---
+++ capstoneProj/Assets/Scripts/InteractablePopup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractablePopup : MonoBehaviour
+{
+    private PopupManager popup;
+    public string displayText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        popup = GameObject.FindGameObjectWithTag("Popup").GetComponent<PopupManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        popup.setText(displayText);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        popup.clearText();
+    }
+}
