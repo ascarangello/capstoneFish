315048cbbfff149a0a69c2ccd9c6259a392ad2a6
ascarangello <33673583+ascarangello@users.noreply.github.com>
2020-10-13 15:20:40 -0400

    press R when dead to restart scene

--- capstoneProj/Assets/Scripts/Character & Camera/CharController.cs
+++ capstoneProj/Assets/Scripts/Character & Camera/CharController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 // Adapted from https://www.youtube.com/watch?v=XhliRnzJe5g
 public class CharController : MonoBehaviour
 {
@@ -30,6 +31,10 @@ public class CharController : MonoBehaviour
         {
             Debug.Log("Escape hit, quitting");
             Application.Quit();
+        }
+        if (Input.GetKey(KeyCode.R) && dead)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
