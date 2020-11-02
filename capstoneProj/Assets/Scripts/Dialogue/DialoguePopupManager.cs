87c3362829443550dca2d77e6aa47d469243adc8
ascarangello <33673583+ascarangello@users.noreply.github.com>
2020-11-02 16:20:11 -0500

    new dialog system i hate my stupid brain and computer, will redesign dialogue ui box for name support

--- capstoneProj/Assets/Scripts/Dialogue/DialoguePopupManager.cs
+++ capstoneProj/Assets/Scripts/Dialogue/DialoguePopupManager.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 public class DialoguePopupManager : MonoBehaviour
 {
     private Text displayText;
+    private Queue<DialogueInfo> GlobalDialogue;
+    private DialogueInfo currentDialogue;
     private Image background;
     // Start is called before the first frame update
     void Start()
     {
+        GlobalDialogue = new Queue<DialogueInfo>();
         background = GetComponent<Image>();
         if(background.IsActive())
         {
@@ -17,6 +20,51 @@ public class DialoguePopupManager : MonoBehaviour
         }
         displayText = GetComponentInChildren<Text>();
         displayText.text = "";
+    }
+
+    public void startDialogue(DialogueInfo[] dialogue)
+    {
+        currentDialogue = null;
+        GlobalDialogue.Clear();
+        foreach (DialogueInfo currDialogue in dialogue)
+        {
+            GlobalDialogue.Enqueue(currDialogue);
+        }
+        nextDialogue();
+
+    }
+
+    public void nextDialogue()
+    {
+        if(GlobalDialogue.Count == 0)
+        {
+            hide();
+        }
+        else
+        {
+            currentDialogue = GlobalDialogue.Dequeue();
+            currentDialogue.resetInfo();
+            displayDialogue();
+        }
+    }
+
+    public void displayDialogue()
+    {
+        if(currentDialogue.sentenceQueue.Count == 0)
+        {
+            nextDialogue();
+        }
+        else
+        {
+            string toDisplay = currentDialogue.sentenceQueue.Dequeue();
+            setText(toDisplay);
+            if (!visible())
+            {
+                show();
+            }
+        }
+
+        
     }
 
     // Update is called once per frame
