d19063d7aba28129c3e821d0283ba966552ab9bf
ascarangello <33673583+ascarangello@users.noreply.github.com>
2020-11-10 22:51:03 -0500

    teleporter and note

---
+++ capstoneProj/Assets/Teleporter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Transform thisExitTransform;
+    public GameObject otherTP;
+    private Transform exitSpot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        exitSpot = otherTP.GetComponent<Teleporter>().thisExitTransform;
+        thisExitTransform.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameObject player = other.gameObject;
+            CharController playerController = player.GetComponent<CharController>();
+            playerController.enabled = false;
+            player.transform.position = exitSpot.position;
+            StartCoroutine(movementEnable(playerController));
+        }
+    }
+    IEnumerator movementEnable(CharController controller)
+    {
+        yield return new WaitForEndOfFrame(); 
+        controller.enabled = true; 
+
+    }
+
+
+}
