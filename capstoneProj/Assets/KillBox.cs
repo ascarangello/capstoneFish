acd46faab91fb6dbe52dd06a0de82d78eb2e55fc
ascarangello <33673583+ascarangello@users.noreply.github.com>
2020-12-08 16:20:12 -0500

    Deathbox restarts if fall off map, fixed demo complete dialogue trigger, added visual connectors between switchpuzzle buttons for visual clarity

---
+++ capstoneProj/Assets/KillBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillBox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
