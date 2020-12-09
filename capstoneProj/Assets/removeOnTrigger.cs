5c47fc1119b5e4eca1d7b3b9d13b5041a44c894c
ascarangello <33673583+ascarangello@users.noreply.github.com>
2020-12-09 10:50:39 -0500

    added new dialogue and starting side room in scene called "added_dialogue"

---
+++ capstoneProj/Assets/removeOnTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class removeOnTrigger : MonoBehaviour
+{
+    public GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            obj.SetActive(false);
+        }
+    }
+}
