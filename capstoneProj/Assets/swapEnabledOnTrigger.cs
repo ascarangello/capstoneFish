0a85f772bc785478a6109914a7e5fa80749f0a0c
ascarangello <33673583+ascarangello@users.noreply.github.com>
2021-02-17 19:57:07 -0500

    set things up for peaceful exploration build

---
+++ capstoneProj/Assets/swapEnabledOnTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swapEnabledOnTrigger : MonoBehaviour
+{
+    public List<GameObject> swapped;
+    public bool once;
+    private bool trigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!once || !trigger)
+        {
+            foreach (GameObject obj in swapped)
+            {
+                obj.SetActive(!obj.activeSelf);
+            }
+            trigger = true;
+        }
+    }
+}
