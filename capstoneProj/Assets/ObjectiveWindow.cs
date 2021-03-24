98a734951641d1c2d305b8a1d3a735312f7ae461
ascarangello <33673583+ascarangello@users.noreply.github.com>
2021-03-23 20:50:56 -0400

    added objective window and trigger component

---
+++ capstoneProj/Assets/ObjectiveWindow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ObjectiveWindow : MonoBehaviour
+{
+    private Text objectiveText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectiveText = GetComponent<Text>();
+        objectiveText.text = "";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setObj(string obj) {
+        objectiveText.text = obj;
+    }
+
+}
---
+++ capstoneProj/Assets/changeObjTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeObjTrigger : MonoBehaviour
+{
+    private bool triggered = false;
+    public string newObjective;
+    public ObjectiveWindow objWindow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!triggered &&other.gameObject.CompareTag("Player"))
+        {
+            triggered = true;
+            objWindow.setObj(newObjective);
+        }
+    }
+}
